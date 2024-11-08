efdda4b9bce8e85cf5a92ef75f329f0f6e6c7b7a
sumit <sumitqwer1234@gmail.com>
2024-11-08 17:50:16 +0530

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Blog = require('./models/blog.model');
 
 
 const app = express()
-const port = process.env.PORT || 8004
+const port = 'https://blogify-2-snt5.onrender.com/'
 
 const userRouter = require('./routes/user');
 const blogRouter = require('./routes/blog');
